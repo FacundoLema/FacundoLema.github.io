a57caf7d7e4782618469b2f9248cbfa4bbf1c019
facundolema <facurlema@gmail.com>
2022-02-12 12:11:18 -0300

    Fixed stuff

--- people/functions.js
+++ people/functions.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let languages = document.querySelectorAll(".language")
     languages.forEach(element => {
         element.addEventListener("mouseover", function() {
-            document.querySelector(".active-language").style.backgroundColor = "#EEE";})
+            document.querySelector(".active").style.backgroundColor = "#EEE";})
         element.addEventListener("mouseout", function() {
-            document.querySelector(".active-language").style.backgroundColor = "#D9D9F5";})
+            document.querySelector(".active").style.backgroundColor = "#D9D9F5";})
         });
 })
